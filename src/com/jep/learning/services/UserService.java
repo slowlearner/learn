03a75a13a9641c93931d2ceda5961b0ddd0c7f87
Erwin Atuli <erwinatuli@gmail.com>
2013-10-13 01:36:53 +0800

    moved serialize fns to util

--- src/com/jep/learning/services/UserService.java
+++ src/com/jep/learning/services/UserService.java
@@ -1,18 +1,13 @@
 package com.jep.learning.services;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import com.jep.learning.models.User;
+import com.jep.learning.util.Serializer;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.util.Base64;
-import android.util.Log;
 
 public class UserService {
 	private Context context;
@@ -23,35 +18,9 @@ public class UserService {
 		this.prefs = prefs;
 	}
 	
-	private String serialize(Object o) {
-		try {
-			ByteArrayOutputStream bo = new ByteArrayOutputStream();
-			ObjectOutputStream so = new ObjectOutputStream(bo);
-			so.writeObject(o);
-			so.flush();
-			return Base64.encodeToString(bo.toByteArray(), Base64.DEFAULT);			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	private Object unserialize(String s) {
-		byte b[] = Base64.decode(s, Base64.DEFAULT);
-		Object o = null;
-		try {
-			ByteArrayInputStream bi = new ByteArrayInputStream(b);
-			ObjectInputStream si = new ObjectInputStream(bi);
-			o = si.readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return o;		
-	}
-	
 	public void saveUsers(ArrayList<User> users) {
 		Editor editor = prefs.edit();
-		editor.putString("users", serialize(users));
+		editor.putString("users", Serializer.serialize(users));
 		editor.commit();
 	}
 	public ArrayList<User> getUsers() {
@@ -60,7 +29,7 @@ public class UserService {
 			return new ArrayList<User>();
 		}
 		
-		ArrayList<User> list = (ArrayList<User>) unserialize(users);
+		ArrayList<User> list = (ArrayList<User>) Serializer.unserialize(users);
 		return list;
 	}
 	
